b082768105927abcc70981dbb404722f54589df8
CyrilThomas23 <cyrilthomaseasow23@gmail.com>
2022-07-30 16:15:00 +0530

    new

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mask_app/SecondScreen.dart';
 
 import 'package:mask_app/main.dart';
 
@@ -28,3 +29,5 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
+
